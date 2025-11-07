69c7bdc11b7e70c3534322a1d52a9f7dc3272322
Prithvee <ojhaprithvee@gmail.com>
2025-11-07 14:14:06 +0530

    add db details

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const port = 3000;
 
 // Create connection to MySQL
 const db = mysql.createConnection({
-    host: 'testdb-1.c34egoce037f.ap-south-1.rds.amazonaws.com',
+    host: 'ci-implementation-db.c3k2mgic8e8l.ap-south-1.rds.amazonaws.com',
     user: 'root',
-    password: '12345678',
-    database: 'testdb_1'
+    password: 'ashimerihai',
+    database: 'ci-implementation-db'
 });
 
 // Connect to MySQL
